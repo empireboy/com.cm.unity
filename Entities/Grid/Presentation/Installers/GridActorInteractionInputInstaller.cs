917f7d281751538fa0bb5d878c4b1b54dca028bf
Casper Meloen <cemeloen@gmail.com>
2026-08-11 17:46:42 +0200

    Fixed an issue that still causes input controllers to not run due to the TickableAdapter not being installed

--- Entities/Grid/Presentation/Installers/GridActorInteractionInputInstaller.cs
+++ Entities/Grid/Presentation/Installers/GridActorInteractionInputInstaller.cs
@@ -1,15 +1,14 @@
-using CM.Unity.Infrastructure;
+using UnityEngine;
 using Zenject;
 
 namespace CM.Unity.Presentation
 {
+    [RequireComponent(typeof(TickableInstaller))]
     public class GridActorInteractionInputInstaller : MonoInstaller
     {
         public override void InstallBindings()
         {
             Container.Bind<Core.Domain.ITickable>().To<GridActorInteractionInputController>().AsSingle();
-
-            Container.BindInterfacesTo<TickableAdapter>().AsSingle();
         }
     }
 }
--- Entities/Grid/Presentation/Installers/GridActorMovementInputInstaller.cs
+++ Entities/Grid/Presentation/Installers/GridActorMovementInputInstaller.cs
@@ -1,15 +1,14 @@
-using CM.Unity.Infrastructure;
+using UnityEngine;
 using Zenject;
 
 namespace CM.Unity.Presentation
 {
+    [RequireComponent(typeof(TickableInstaller))]
     public class GridActorMovementInputInstaller : MonoInstaller
     {
         public override void InstallBindings()
         {
             Container.Bind<Core.Domain.ITickable>().To<GridActorMovementInputController>().AsSingle();
-
-            Container.BindInterfacesTo<TickableAdapter>().AsSingle();
         }
     }
 }
