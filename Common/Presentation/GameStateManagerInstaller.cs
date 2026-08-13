0db261e7b2298236d3ee1bfacdd6021803269688
Casper Meloen <cemeloen@gmail.com>
2026-08-13 18:01:04 +0200

    Fixed GameStateManager not being installed with a game state

--- Common/Presentation/GameStateManagerInstaller.cs
+++ Common/Presentation/GameStateManagerInstaller.cs
@@ -1,18 +1,14 @@
 using CM.Core.Application;
 using CM.Core.Domain;
-using UnityEngine;
 using Zenject;
 
 namespace CM.Unity.Presentation
 {
     public class GameStateManagerInstaller : MonoInstaller
     {
-        [SerializeField]
-        private GameState _startGameState;
-
         public override void InstallBindings()
         {
-            Container.BindInstance(new GameStateManager(_startGameState));
+            Container.BindInstance(new GameStateManager(GameStates.Gameplay));
         }
     }
 }
